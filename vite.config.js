c06c90f1bc5ad4ccdb073f7c2b8038fc1c079e42
David Majholt <d.boy.dragoz@gmail.com>
2025-11-04 11:55:30 +0100

    Major bug fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tic-tac-toe-katytye/',
+  base: '/tic-tac-toe-KatyTye/',
   build: {
     outDir: 'docs',
   }
